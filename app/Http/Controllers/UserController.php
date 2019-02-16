ac539ef6f2e27797ec5588b50818533bebb70b7e
nisaandriyani <nisaandriyani46@gmail.com>
2019-02-16 11:11:39 +0700

    episode 12

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,17 @@ class UserController extends Controller
 {
     public function daftar(Request $req)
     {
-    	$data = User::where('name','like',"%{$req->keyword}%")->paginate(10);
-    	return view('admin.pages.user.daftar',['data'=>$data]);
+        $data = User::where('name','like',"%{$req->keyword}%")->paginate(10);
+        return view('admin.pages.user.daftar',['data'=>$data]);
+    }
+
+    public function add()
+    {
+        return view('admin.pages.user.add');
+    }
+
+    public function save(Request $req)
+    {
+        return 'Fungsi Save';
     }
 }
